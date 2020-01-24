9649dcb4907f577db6055cb48231aa9bc9dc763b
peterbatridge <11344363+peterbatridge@users.noreply.github.com>
2020-01-23 21:49:17 -0600

    trying software spi

--- nonagon.py
+++ nonagon.py
@@ -23,9 +23,11 @@ from firebase_admin import firestore
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
-SPI_PORT = 0
-SPI_DEVICE = 0
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Use a service account
 cred = credentials.Certificate('firestoreNonagon.json')
